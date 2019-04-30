13993867bed629d3feba54cf9a9c904ce7866376
Jonny Graybill <jon.a.graybill@gmail.com>
2019-04-29 20:51:55 -0700

    finally working

--- server.js
+++ server.js
@@ -125,40 +125,47 @@ function Weather(day) {
 
 
 function getEvents(request, response) {
-  const sql = `SELECT * FROM events WHERE location_id=$1;`;
-  const values = [request.query.data.id];
+  let query = request.query.data.id;
+  let sql = `SELECT * FROM events WHERE location_id=$1;`;
+  let values = [query];
 
-  return client.query(sql, values)
+  client.query(sql, values)
     .then(result => {
-      if (result.rowCount > 0) {
-        response.send(result.rows[0]);
+      if(result.rowCount > 0) {
+        response.send(result.rows);
       } else {
-        const url = `https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.longitude=${request.query.data.longitude}&location.latitude=${request.query.data.latitude}&expand=venue`;
+        const url = `https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.address=${request.query.data.formatted_query}`;
 
-        superagent.get(url)
+        return superagent.get(url)
           .then(result => {
-            const event = result.body.events.map(events => {
-              const event = new Event(events);
-              return event;
-            });
-            let newSQL = `INSERT INTO meetups(link, name, event_date, location_id ) VALUES ($1, $2, $3, $4, $5);`;
+            if (!result.body.events.length) { throw 'NO DATA'; }
+            else {
+              const eventSummaries = result.body.events.map(events => {
+                let event = new Event(events);
+                event.id = query;
 
-            let newValues = Object.values(event);
-            return client.query(newSQL, newValues)
+                let newSQL = `INSERT INTO events (link, name, event_date, summary, location_id) VALUES ($1, $2, $3, $4, $5);`;
+                let newValues = Object.values(event);
+
+                client.query(newSQL, newValues);
+
+                return event;
+              });
+              response.send(eventSummaries.slice(0, 20));
+            }
           })
-          .catch(err => handleError(err, response));
-        response.send(event.slice(0, 20));
+          .catch(error => handleError(error, response));
       }
-    })
+    });
 }
 
 
 //eventbrite constructor
-function Event(events) {
-  this.link = events.url;
-  this.name = events.name.text;
-  this.event_date = new Date(events.start.local).toDateString();
-  this.summary = events.summary;
+function Event(event) {
+  this.link = event.url;
+  this.name = event.name.text;
+  this.event_date = new Date(event.start.local).toDateString();
+  this.summary = event.summary;
 }
 
 //Error handler
